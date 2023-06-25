538cbaf112cfa2567135d76c16231a0b91b5aae7
Elitsa Dacheva <eli4ka753@abv.bg>
2023-06-25 16:02:14 +0300

    new exercise

---
+++ Encapsulation/FirstAndReserveTeam/Person.java
@@ -0,0 +1,79 @@
+package OOPLab.Encapsulation.FirstAndReserveTeam;
+
+import java.text.DecimalFormat;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.age < 30) {
+            this.salary = this.salary + (this.salary * bonus / 200);
+        } else {
+            this.salary = this.salary + (this.salary * bonus / 100);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        DecimalFormat myFormatter = new DecimalFormat("0.0##");
+        String output = myFormatter.format(this.salary);
+        return String.format("%s %s gets %s leva", this.firstName, this.lastName, output);
+    }
+}
---
+++ Encapsulation/ValidationData/Main.java
@@ -0,0 +1,8 @@
+package OOPLab.Encapsulation.ValidationData;
+
+
+public class Main {
+    public static void main(String[] args)   {
+
+    }
+}
