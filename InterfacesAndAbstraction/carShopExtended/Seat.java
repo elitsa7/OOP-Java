eb51b86cdfb1cacd9c34bbfd12996646726c24eb
Elitsa Dacheva <eli4ka753@abv.bg>
2023-07-03 17:54:42 +0300

    new exercise

---
+++ InterfacesAndAbstraction/carShopExtended/Seat.java
@@ -0,0 +1,19 @@
+package carShopExtended;
+
+public class Seat extends CarImpl implements Sellable{
+    private Double price;
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format("%s sells for %f", getModel(), getPrice());
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+}
